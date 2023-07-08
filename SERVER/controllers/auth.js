784375fb0dd6b1669c61da1f82fd4b71e3f34b12
Parth <parthvaghela791@gmail.com>
2023-07-09 02:34:10 +0530

    otp verification through mail

---
+++ SERVER/controllers/auth.js
@@ -0,0 +1,4 @@
+//sign up
+//login
+//send otp
+//change password
--- SERVER/models/otp.js
+++ SERVER/models/otp.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
+const { isAsyncFunction } = require("util/types");
+const { functions } = require("lodash");
 const OTPSchema = new mongoose.Schema({
     email:{
         type:String,
@@ -14,5 +17,20 @@ const OTPSchema = new mongoose.Schema({
         expires:5*60,
     }
 });
-
+//a fucntion to send mail
+async function sendVerificationEmail(email,otp){
+try{
+const mailResponse = await mailSender(email,"Verfication email from studynotion",otp);
+console.log("email send succesfully",mailResponse);
+}
+catch(error){
+    console.log("error occured while sending mails:",error);
+    throw error;
+}
+}
+// using pre hook from mongoose also known as middleware
+OTPSchema.pre("save",async function(next){
+    await sendVerificationEmail(this.email,this.otp);
+    next();
+})
 module.exports = mongoose.model("OTP",OTPSchema);
