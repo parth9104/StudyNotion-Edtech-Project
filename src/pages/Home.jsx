5d10dd5e69d067890b6d0be3d0899964d95a6d8c
Parth <parthvaghela791@gmail.com>
2023-07-18 22:42:15 +0530

    react started

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import react from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div>
+            {/*section 01*/}
+            <div>
+                <Link to={"/signup"}>
+                    <div>
+                        <div>
+                            <p>Become an instructor</p>
+                        </div>
+                    </div>
+                    </Link>
+
+            </div>
+        
+    {/*section 02*/ }
+    {/*section 03*/ }
+    {/*Footer*/ }
+
+</div>
+        )
+}
+export default Home;
